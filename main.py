b455f1000da6951e5eaade884dccdc494f1a03cb
mitsy01 <alexanderfigmochka19@gmail.com>
2025-03-12 21:38:13 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Query
+import uvicorn
+
+import db
+
+app = FastAPI()
+users = []
+
+@app.get("/get_users/")
+def get_users():
+    return {"users": users}
+
+@app.post("/add_user/")
+def add_user(name: str = Query()):
+    if name not in db.data:
+        users.append(name)
+        return dict(msg="Дані додано.")
+    
+@app.delete("/del_user/")
+def del_user(name: str = Query()):
+    if name not in users:
+        return dict(msg="Немає такого імя")
+    users.remove(name)
+    return dict(msg="")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")
